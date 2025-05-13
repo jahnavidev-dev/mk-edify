49d5b28b762f24e7cab2c72b949710d46d5257f3
muralialakuntla3 <muralialakuntla3@gmail.com>
2025-05-13 11:57:13 +0530

    ui change in frontpage

--- src/components/FrontPage.js
+++ src/components/FrontPage.js
@@ -1,12 +1,14 @@
-// src/components/FrontPage.js
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './FrontPage.css'; // Optional for styling
 
-const FrontPage = () => (
-  <div>
-    <h1>Welcome to MK Tech</h1>
-    <Link to="/admin">Go to Admin Portal</Link>
-  </div>
-);
+const FrontPage = () => {
+  return (
+    <div className="frontpage-container">
+      <h1 className="title">Welcome to MK Tech</h1>
+      <Link to="/admin" className="admin-link">Admin Portal</Link>
+    </div>
+  );
+};
 
 export default FrontPage;
